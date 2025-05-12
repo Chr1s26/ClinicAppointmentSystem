f604e6cc3d23f98c4a870493966ac027a2a56084
Chris <htetkyawswarlinn44@gmail.com>
2025-05-12 18:03:28 +0700

    uploaded

---
+++ src/main/java/com/clinic/appointment/service/DoctorService.java
@@ -0,0 +1,38 @@
+package com.clinic.appointment.service;
+
+import com.clinic.appointment.model.Doctor;
+import com.clinic.appointment.repository.DoctorRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@AllArgsConstructor
+public class DoctorService {
+
+    private final DoctorRepository doctorRepository;
+
+    public Doctor createDoctor(Doctor doctor){
+        doctor =this.doctorRepository.save(doctor);
+        return doctor;
+    }
+
+    public Doctor updateDoctor(Doctor doctor){
+        doctor =this.doctorRepository.save(doctor);
+        return doctor;
+    }
+
+    public List<Doctor> getDoctors(){
+        List<Doctor> doctors =this.doctorRepository.findAll();
+        return doctors;
+    }
+
+    public Doctor getDoctorById(Long id){
+        return this.doctorRepository.findById(id).orElseThrow();
+    }
+
+
+
+}
